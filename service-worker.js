d4754c5a1782c3879036c96a59d881c102594fbe
agendouja <globalsixizidoro@gmail.com>
2025-05-23 19:01:46 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,16 @@
-
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('agendouja').then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json'
-      ]);
-    })
-  );
+self.addEventListener('install', (event) => {
+  console.log('Service Worker instalado');
+  self.skipWaiting();
 });
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
+
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker ativado');
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return new Response('Você está offline. Conecte-se à internet para usar o app.');
     })
   );
 });
